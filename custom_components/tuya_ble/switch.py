8c6c4a7d021e621e753c9aacf3dc791169e799c2
glendower <53557300+glendower@users.noreply.github.com>
2025-01-17 21:22:09 +0000

    Update switch.py

--- custom_components/tuya_ble/switch.py
+++ custom_components/tuya_ble/switch.py
@@ -232,6 +232,7 @@ mapping: dict[str, TuyaBLECategorySwitchMapping] = {
                     "bnt7wajf",
                     "rvdceqjh",
                     "5xhbk964",
+                    "riecov42"
                 ],  # Fingerbot
                 [
                     TuyaBLEFingerbotSwitchMapping(dp_id=2),
